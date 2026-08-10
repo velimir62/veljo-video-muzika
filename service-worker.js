48ed797e48433dccc4ecf383d16180df6ee075ff
velimir62 <velimir62@gmail.com>
2026-08-09 18:17:15 -0600

    Replace processed with Videos

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "veljo-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./playlist.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
+  );
+});
